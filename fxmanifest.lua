8caa1191c7293ae6be7913a5b4ef12dda78c9991
Tigo <me@tigodev.com>
2020-06-09 14:23:12 +0200

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'FXCore Framework'
+name 'FXCore'
+author 'TigoDevelopment'
+contact 'me@tigodev.com'
+
+version '1.0.0'
+
+server_scripts {
+    'server/common.lua',
+    'server/module.lua',
+    'server/main.lua'
+}
